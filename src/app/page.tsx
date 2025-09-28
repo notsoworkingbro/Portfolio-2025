7e97f9b2a525f0578e1963ded9db409d620d57cc
notsoworkingbro <workingamap@gmail.com>
2025-09-28 18:33:20 +0800

    change the layout and the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-red-500">
-      <h1 className="text-5xl font-bold text-white">Hello Tailwind!</h1>
-    </main>
+    <>
+      <Hero />
+      <Projects />
+      <Contact />
+    </>
   );
 }
