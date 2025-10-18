d3d0078ca44c95939732993b4a44b9ac53399904
notsoworkingbro <workingamap@gmail.com>
2025-10-18 15:00:40 +0800

    added motion-framer to cardlogos

--- src/components/Cardlogos.tsx
+++ src/components/Cardlogos.tsx
@@ -7,28 +7,44 @@ interface CardProps {
 }
 
 const Cardlogos: React.FC<CardProps> = ({ image }) => {
-    const [showOverlay, setShowOverlay] = useState(true);
+    const [showOverlay, setShowOverlay] = useState(false);
 
     return (
         <motion.div 
-            className="relative overflow-hidden h-[200px] min-w-[200px] bg-slate-400 rounded-xl flex justify-center items-center"> {/* parent must be relative & sized */}
-
+            className="relative overflow-hidden h-[200px] min-w-[200px] bg-slate-400 rounded-xl flex justify-center items-center"
             
+            onHoverStart = {() => setShowOverlay(true)}
+            onHoverEnd = {() => setShowOverlay(false)}
+            
+            > 
 
             { showOverlay && (
-                <div className="absolute inset-0 z-10 flex justify-center items-center">
+                <motion.div className="absolute inset-0 z-10 flex justify-center items-center"
+                
+                initial = {{ opacity: 0 }}
+                animate = {{ opacity: 1 }}
+                exit    = {{ opacity: 0 }}
+
+                >
                     <div className="absolute bg-black pointer-events-none opacity-50 h-full w-full"></div>
-                    <h1 className="bg-white font-semibold text-sm z-10 px-3 py-2 rounded-full flex items-center gap-[0.5ch] hover:opacity-75">
+
+                    <motion.h1 className="bg-white font-semibold text-sm z-10 px-3 py-2 rounded-full flex items-center gap-[0.5ch] hover:opacity-75"
+                    
+                    initial = {{ opacity: 10 }}
+                    animate = {{ opacity: 0  }}
+                    exit    = {{ opacity: 10 }}
+
+                    >
                         <span>Explore Now</span>
-                    </h1>
-                </div>
+                    </motion.h1>
+                </motion.div>
             )}
             <Image
                 src={image}
                 alt={image}
                 fill
-                style={{ objectFit: "cover" }}  // ✅ fixed syntax
-                className="opacity-80 hover:opacity-100 transition"
+                style={{ objectFit: "cover" }}  
+                
             />
         </motion.div>
     );
