005395009cec72a16a52ca08259d6080ea2a7693
notsoworkingbro <workingamap@gmail.com>
2025-10-18 22:11:11 +0800

    fixed the marquee animation and added another animation duration to wait for, so that animation will not reset

--- src/components/LogoMarquee.tsx
+++ src/components/LogoMarquee.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import Cardlogos from "@/components/Cardlogos"
+import { animate, motion, useMotionValue } from "framer-motion";
+import { useState, useEffect } from "react";
+import useMeasure from "react-use-measure";
 
 export default function LogoMarquee() {
   const images = [
@@ -13,11 +16,61 @@ export default function LogoMarquee() {
     "/logos/python-127-svgrepo-com.svg",
   ];
 
+  const FAST_DURATION = 25;
+  const SLOW_DURATION = 75;
+
+  const [duration, setDuration] = useState(FAST_DURATION)
+
+  const [ref, { width }] = useMeasure();
+
+  const xTranslation = useMotionValue(0);
+
+  const [mustFinish, setMustFinish] = useState(false);
+  const [rerender, setRerender]     = useState(false);
+
+  useEffect(() => {
+    let controls;
+    const finalPosition = -width / 2 - 8;
+
+    if (mustFinish) {
+      controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
+        ease: 'linear',
+        duration: duration * ( 1 - xTranslation.get()/ finalPosition),
+        onComplete: () => {
+          setMustFinish(false);
+          setRerender(!rerender);
+        },
+      });
+    } else {
+      controls = animate(xTranslation, [0, finalPosition], {
+        ease: "linear",
+        duration: duration,
+        repeat: Infinity,
+        repeatType: "loop",
+        repeatDelay: 0,
+      });
+    }
+
+    return controls.stop;
+  }, [xTranslation, width, duration, rerender]);
+
   return <main className="py-8">
-    <div className="absolute left-0 flex gap-4">
-      {[...images].map((item, idx) => (
+    <motion.div 
+      className="absolute left-0 flex gap-4" 
+    
+      ref={ref}
+      style = {{ x:xTranslation }}
+      onHoverStart = {() => { 
+        setMustFinish(true);
+        setDuration(SLOW_DURATION)
+      }}
+      onHoverEnd   = {() => {
+        setMustFinish(true);
+        setDuration(FAST_DURATION)}}
+      >
+      {[...images, ...images].map((item, idx) => (
         <Cardlogos image={item} key={idx}/>
       ))}
-    </div>
+    </motion.div>
   </main>
 }
