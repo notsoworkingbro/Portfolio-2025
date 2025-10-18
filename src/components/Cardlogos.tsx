a30fd639d16167610b5d02edc6cac6199dffefbd
notsoworkingbro <workingamap@gmail.com>
2025-10-18 15:11:35 +0800

    Fixed hovering animation on CardLogos

--- src/components/Cardlogos.tsx
+++ src/components/Cardlogos.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface CardProps {
     image: string;
@@ -18,27 +18,30 @@ const Cardlogos: React.FC<CardProps> = ({ image }) => {
             
             > 
 
-            { showOverlay && (
-                <motion.div className="absolute inset-0 z-10 flex justify-center items-center"
-                
-                initial = {{ opacity: 0 }}
-                animate = {{ opacity: 1 }}
-                exit    = {{ opacity: 0 }}
-
-                >
-                    <div className="absolute bg-black pointer-events-none opacity-50 h-full w-full"></div>
-
-                    <motion.h1 className="bg-white font-semibold text-sm z-10 px-3 py-2 rounded-full flex items-center gap-[0.5ch] hover:opacity-75"
+            <AnimatePresence>
+                { showOverlay && (
+                    <motion.div className="absolute inset-0 z-10 flex justify-center items-center"
                     
-                    initial = {{ opacity: 10 }}
-                    animate = {{ opacity: 0  }}
-                    exit    = {{ opacity: 10 }}
+                    initial = {{ opacity: 0 }}
+                    animate = {{ opacity: 1 }}
+                    exit    = {{ opacity: 0 }}
 
                     >
-                        <span>Explore Now</span>
-                    </motion.h1>
-                </motion.div>
-            )}
+                        <div className="absolute bg-black pointer-events-none opacity-50 h-full w-full"></div>
+
+                        <motion.h1 className="bg-white font-semibold text-sm z-10 px-3 py-2 rounded-full flex items-center gap-[0.5ch] hover:opacity-75"
+                        
+                        initial = {{ y: 10 }}
+                        animate = {{ y: 0  }}
+                        exit    = {{ y: 10 }}
+
+                        >
+                            <span>Explore Now</span>
+                        </motion.h1>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+
             <Image
                 src={image}
                 alt={image}
